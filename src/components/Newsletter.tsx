455b6a2a0b0652090a2ed1b44b3c4d727f109a26
Vinicius Pacheco <vinicius@pachecoof@hotmail.com>
2022-07-21 22:28:08 -0300

    Adicionado validação de e-mail

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -1,4 +1,17 @@
+import { useState } from "react";
+import validator from 'validator'
+
 const Newsletter = () => {
+  const [email, setEmail] = useState('');
+  const [emailValido, setEmailValido] = useState(false);
+
+  function adicionarEValidarEmail(event: React.ChangeEvent<HTMLInputElement>) {
+    const email = event.target.value;
+
+    setEmail(email);
+    setEmailValido(validator.isEmail(email));
+  }
+
   return (
     <div className="flex gap-10">
       <div className="max-w-lg mt-36 flex flex-col gap-4">
@@ -7,8 +20,8 @@ const Newsletter = () => {
         <p className="text-gray-500">Encontre aqui uma vasta seleção de plantas para decorar a sua casa e torná-lo uma pessoa mais feliz no seu dia a dia. Entre com seu e-mail e assine nossa newsletter para saber das novidades da marca.</p>
 
         <div className="flex flex-row">
-          <input className="w-full h-16 px-6 shadow-sm" placeholder="Insira seu e-mail" type="text" />
-          <button className="w-72 bg-yellow-400 text-white text-sm">Assinar newslatter</button>
+          <input onChange={(event) => adicionarEValidarEmail(event)} className="w-full h-16 px-6 shadow-sm" placeholder="Insira seu e-mail" type="email" />
+          <button disabled={!emailValido} className="w-72 bg-yellow-400 text-white text-sm disabled:opacity-50">Assinar newslatter</button>
         </div>
       </div>
 
