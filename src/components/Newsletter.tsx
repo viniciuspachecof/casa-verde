e4c5844adfaeacdc4341ac86d443634d039241bf
Vinicius Pacheco <vinicius@pachecoof@hotmail.com>
2022-07-22 22:32:36 -0300

    Alerta de confirmação de envio de email

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -5,11 +5,20 @@ const Newsletter = () => {
   const [email, setEmail] = useState('');
   const [emailValido, setEmailValido] = useState(false);
 
-  function adicionarEValidarEmail(event: React.ChangeEvent<HTMLInputElement>) {
+  function validarEmail(event: React.ChangeEvent<HTMLInputElement>) {
     const email = event.target.value;
 
     setEmail(email);
     setEmailValido(validator.isEmail(email));
+  }
+
+  function enviarFormulario(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    alert(`Obrigado pela sua assinatura, você receberá nossas novidades no e-mail ${email}`);
+
+    setEmail('');
+    setEmailValido(false);
   }
 
   return (
@@ -19,13 +28,13 @@ const Newsletter = () => {
         <h1 className="font-bold text-[82px] leading-[94px] font-elsie">melhores plantas</h1>
         <p className="text-gray-500">Encontre aqui uma vasta seleção de plantas para decorar a sua casa e torná-lo uma pessoa mais feliz no seu dia a dia. Entre com seu e-mail e assine nossa newsletter para saber das novidades da marca.</p>
 
-        <div className="flex flex-row">
-          <input onChange={(event) => adicionarEValidarEmail(event)} className="w-full h-16 px-6 shadow-sm" placeholder="Insira seu e-mail" type="email" />
-          <button disabled={!emailValido} className="w-72 bg-yellow-400 text-white text-sm disabled:opacity-50">Assinar newslatter</button>
-        </div>
+        <form method="post" onSubmit={enviarFormulario} className="flex flex-row">
+          <input value={email} onChange={(event) => validarEmail(event)} className="w-full h-16 px-6 shadow-sm" placeholder="Insira seu e-mail" type="email" />
+          <button type="submit" disabled={!emailValido} className="w-72 bg-yellow-400 text-white text-sm disabled:opacity-50">Assinar newslatter</button>
+        </form>
       </div>
 
-      <img src="/assets/imagem-planta.png" alt="imagem de uma planta" className="mt-7 w-[450px]"/>
+      <img src="/assets/imagem-planta.png" alt="imagem de uma planta" className="mt-7 w-[450px]" />
     </div>
 
   )
